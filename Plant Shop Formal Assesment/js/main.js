71cf3608ced61cfc25b8f0cdbeb0464fff341c85
KiraGroenewaldOW <125360966+Kira-G-OW@users.noreply.github.com>
2023-08-02 19:31:08 +0200

    Add files via upload

---
+++ Plant Shop Formal Assesment/js/main.js
@@ -0,0 +1,12 @@
+
+//When the document loads 
+
+$(document).ready(function(){
+
+    console.log("Hello World!");
+
+    // When the hero image loads, animate it upwards
+
+    $(".hero-image").animate({top: '-=100px'})
+
+});
