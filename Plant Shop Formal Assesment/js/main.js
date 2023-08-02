03baa0e7e313ac371251b1007892ee7baeb5437d
KiraGroenewaldOW <125360966+Kira-G-OW@users.noreply.github.com>
2023-08-02 22:57:20 +0200

    Add files via upload

--- Plant Shop Formal Assesment/js/main.js
+++ Plant Shop Formal Assesment/js/main.js
@@ -5,8 +5,49 @@ $(document).ready(function(){
 
     console.log("Hello World!");
 
-    // When the hero image loads, animate it upwards
+    // ----------------------------------------------------------------
+    // Homepage
 
+    // When the hero image loads, animate it upwards
     $(".hero-image").animate({top: '-=100px'})
 
+
+    // ---------------------------------------------------------------
+    // Browse
+
+    $("#descriptionText").hide();
+
+
+
+
+    // ---------------------------------------------------------------
+    // Wishlist
+
 });
+
+
+// When the card is clicked
+$(".card").click(function(){
+
+    // toggle price and description
+    $("#priceText").toggle();
+    $("#descriptionText").toggle();
+
+    // Resize the image to fit the additional content 
+    $(".card-img-top").toggleClass("small");
+})
+
+
+// when "Remove" button is clicked
+
+$("#btn-1").click(function (){
+    $(".row-1").hide("slow");
+})
+
+$("#btn-2").click(function (){
+    $(".row-2").hide("slow");
+})
+
+$("#btn-3").click(function (){
+    $(".row-3").hide("slow");
+})
